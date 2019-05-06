7b1daf99a259adc47192eef37299cf5a50510450
GS74390E2 <47746189+GS74390E2@users.noreply.github.com>
2019-05-06 14:47:21 +0800

    Update urls.py

--- Djiuwei/activity/urls.py
+++ Djiuwei/activity/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from activity.views import CreateActivity, MyActivity, HistoryActivity, TojoinActivity, ChangeActivity, QuitActivity
+from activity.views import CreateActivity, MineActivity, HistoryActivity, TojoinActivity, ChangeActivity, QuitActivity
 
 app_name = "activity"
 urlpatterns = [
 	path('createActivity', CreateActivity.as_view(), name="createActivity"),
-	path('myActivity', MyActivity.as_view(), name="myActivity"),
+	path('mineActivity', MineActivity.as_view(), name="mineActivity"),
 	path('historyActivity', HistoryActivity.as_view(), name="historyActivity"),
-	path('tojoinActivity', TojoinActivity.as_view(), name="tojoinActivity"),
+	path('toJoinActivity', ToJoinActivity.as_view(), name="toJoinActivity"),
 	path('changeActivity', ChangeActivity.as_view(), name="changeActivity"),
 	path('quitActivity', QuitActivity.as_view(), name="quitActivity"),
 ]
